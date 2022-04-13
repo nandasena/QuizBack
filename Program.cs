b023dd904fb626336e2675bc4d5c88b6764a07a7
CA\SamithaNandasena <samitha.nandasena@cammsgroup.com>
2022-04-13 20:53:33 +0530

    init Commit

--- Program.cs
+++ Program.cs
@@ -6,6 +6,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy("Cors", builder =>
+{
+    builder
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
@@ -15,6 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("Cors");
 
 app.UseHttpsRedirection();
 
